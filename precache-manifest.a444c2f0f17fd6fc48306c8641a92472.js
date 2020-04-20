e9d0fa7cffc1a270860a10467a5ef96a05f00664
Erdenebulgan <erdenebulgandeveloper@gmail.com>
2020-04-20 19:56:11 +0800

    Updates

---
+++ precache-manifest.a444c2f0f17fd6fc48306c8641a92472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49c5d5b47e40ae229bb625198e30e35",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "0b5a36a4f22d10016202",
+    "url": "/burger/static/css/main.489210e3.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5a36a4f22d10016202",
+    "url": "/burger/static/js/main.b72b8272.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
